623f29ada6b8a8362280ff9e6e41aeb4119acaf5
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-29 21:56:57 +0530

    [Vinayak] adding method to create stack

---
+++ StackAndQueue/StackAndQueue/LinkedListStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class LinkedListStack
+    {
+        public Node top;
+        /// add element into stack.      
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = this.top;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        /// display the stack elements.
+        public void Display()
+        {
+            Node temp = this.top;
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine("Data in Stack :" + temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
